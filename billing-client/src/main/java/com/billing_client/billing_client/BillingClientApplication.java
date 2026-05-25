83c5f5fec5dbc00a641ff27d3134a443bff05ed5
alejoalbornoz <alejoalbornoz912@gmail.com>
2026-05-25 15:46:29 -0300

    Update: Add TimeZone:UTC

--- billing-client/src/main/java/com/billing_client/billing_client/BillingClientApplication.java
+++ billing-client/src/main/java/com/billing_client/billing_client/BillingClientApplication.java
@@ -3,11 +3,16 @@ package com.billing_client.billing_client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class BillingClientApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BillingClientApplication.class, args);
+
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+
+        SpringApplication.run(BillingClientApplication.class, args);
 	}
 
 }
